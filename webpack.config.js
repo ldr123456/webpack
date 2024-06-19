332da0b64e25a33f52c76c97aff3b2e9681a73a2
ldr <2574499130@qq.com>
2024-06-19 19:46:15 +0800

    first

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// import { Configuration } from "webpack";
+/*
+ * @type {Configuration}
+ */
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  mode: 'none',
+}
+module.exports = config
