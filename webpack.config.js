32cb90abebc24a0c106f8ef2948745fecd7558e4
ldr <2574499130@qq.com>
2024-06-19 20:08:03 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,22 @@ const config = {
     filename: 'bundle.js'
   },
   mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
 }
 module.exports = config
